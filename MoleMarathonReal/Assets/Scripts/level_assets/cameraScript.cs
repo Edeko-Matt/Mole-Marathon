c45953adbdd1931ea35acdf34ad8fca1e73d4402
Edeko-Matt <63946995+Edeko-Matt@users.noreply.github.com>
2020-05-16 15:50:56 +1200

    All Changes

---
+++ MoleMarathonReal/Assets/Scripts/level_assets/cameraScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothspeed = 0f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0f, 0f, -10f);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothspeed);
+        transform.position = target.position + offset;
+
+    }
+}
